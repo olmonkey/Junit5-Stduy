d64f58503883cf4b536ec74ae48b09c606e5464e
Seung-ryong-Lee <67668106+Seung-ryong-Lee@users.noreply.github.com>
2023-02-26 13:45:37 +0900

    init Junit5 Study

---
+++ src/main/java/com/study/junit5study/Junit5StudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.junit5study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5StudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/junit5study/Junit5StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.junit5study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
